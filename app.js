03750a7e24ad20d70063a726955ebc35c95ad907
Camille VINCENT <camille.vincent2025@campus-eni.fr>
2025-07-29 16:42:47 +0200

    s004

--- app.js
+++ app.js
@@ -6,15 +6,18 @@ require("dotenv").config()
 
 const port = process.env.PORT_NO || 3000;
 
+app.use(express.static('public'))
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
 let message = "Hello World"
 
 app.get('/', (req, res) => {
-    res.render("index", {message: message })
+    res.render("liste-tickets", {message: message })
   })
 
   app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
   })
+
+  
